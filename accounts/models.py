5a4bbfe08bc36fd9293830aaab1ca49b3262bc0b
QuantifaiPortfol-io <quantifai.io@outlook.com>
2020-08-14 22:00:16 +0800

    Added dynamic url routing and templating

--- accounts/models.py
+++ accounts/models.py
@@ -27,7 +27,7 @@ class Product(models.Model):
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     description = models.CharField(max_length=200, null=True, blank=True)
-    day_created = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField(Tag)
 
     def __str__(self):
@@ -42,6 +42,6 @@ class Order(models.Model):
 
     customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
     product = models.ForeignKey(Product,null=True, on_delete=models.SET_NULL) 
-    day_created = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
     
