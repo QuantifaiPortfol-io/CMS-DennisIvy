bb82f57eaf0a02dee17e01039a45df8703788486
QuantifaiPortfol-io <quantifai.io@outlook.com>
2020-08-18 09:08:03 +0800

    Copied app level source code

--- accounts/views.py
+++ accounts/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth.models import Group
 
 # Create your views here.
 from .models import *
-from .forms import OrderForm, CreateUserForm
+from .forms import OrderForm, CreateUserForm, CustomerForm
 from .filters import OrderFilter
 from .decorators import unauthenticated_user, allowed_users, admin_only
 
@@ -28,9 +28,11 @@ def registerPage(request):
 
 			group = Group.objects.get(name='customer')
 			user.groups.add(group)
+			#Added username after video because of error returning customer name if not added
 			Customer.objects.create(
-				user = user,
-			)
+				user=user,
+				name=user.username,
+				)
 
 			messages.success(request, 'Account was created for ' + username)
 
@@ -84,13 +86,33 @@ def home(request):
 @allowed_users(allowed_roles=['customer'])
 def userPage(request):
 	orders = request.user.customer.order_set.all()
+
 	total_orders = orders.count()
 	delivered = orders.filter(status='Delivered').count()
 	pending = orders.filter(status='Pending').count()
-	
 
-	context = {'orders':orders,'total_orders':total_orders,'delivered':delivered,'pending':pending}
+	print('ORDERS:', orders)
+
+	context = {'orders':orders, 'total_orders':total_orders,
+	'delivered':delivered,'pending':pending}
 	return render(request, 'accounts/user.html', context)
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+	customer = request.user.customer
+	form = CustomerForm(instance=customer)
+
+	if request.method == 'POST':
+		form = CustomerForm(request.POST, request.FILES,instance=customer)
+		if form.is_valid():
+			form.save()
+
+
+	context = {'form':form}
+	return render(request, 'accounts/account_settings.html', context)
+
+
 
 
 @login_required(login_url='login')
@@ -136,11 +158,11 @@ def createOrder(request, pk):
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['admin'])
 def updateOrder(request, pk):
-
 	order = Order.objects.get(id=pk)
 	form = OrderForm(instance=order)
-
+	print('ORDER:', order)
 	if request.method == 'POST':
+
 		form = OrderForm(request.POST, instance=order)
 		if form.is_valid():
 			form.save()
